8f4e23a698833a202b9886447ea1acb62517e52a
William Berman <wlbberman@gmail.com>
2017-12-19 18:49:59 -0800

    pass locals itself not as a hash to render_wrapper

--- app/controllers/generic_api_rails/rest_controller.rb
+++ app/controllers/generic_api_rails/rest_controller.rb
@@ -56,10 +56,10 @@ module GenericApiRails
         if template_or_jbuilder_exists?(tmpl="#{GAR}/#{ model.new.to_partial_path.pluralize }")
         locals = {}
         locals[@model.model_name.element.pluralize] = rows
-        render_wrapper tmpl, locals: locals
+        render_wrapper tmpl, locals
         true
       elsif template_or_jbuilder_exists?(tmpl="#{GAR}/base/collection")
-        render_wrapper tmpl, locals: { collection: rows }
+        render_wrapper tmpl, { collection: rows }
         true
       else
         false
@@ -71,10 +71,10 @@ module GenericApiRails
       if template_or_jbuilder_exists?(tmpl="#{GAR}/#{ row.to_partial_path }")
         locals = {}
         locals[model.model_name.element.to_sym] = row
-        render_wrapper tmpl, locals: locals
+        render_wrapper tmpl, locals
         true
       elsif template_or_jbuilder_exists?(tmpl="#{GAR}/base/item")
-        render_wrapper tmpl, locals: { item: row }
+        render_wrapper tmpl, { item: row }
         true
       else
         false
